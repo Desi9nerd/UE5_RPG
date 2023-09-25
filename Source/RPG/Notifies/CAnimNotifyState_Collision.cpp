5184eb01125e67d479b2fe3dc32341e50de0751d
Desi9nerd <swpark514@gmail.com>
2023-09-25 23:00:06 +0900

    CAnimNotifyState_Collsion - 충돌 시 Blood 및 Effect

--- Source/RPG/Notifies/CAnimNotifyState_Collision.cpp
+++ Source/RPG/Notifies/CAnimNotifyState_Collision.cpp
@@ -32,4 +32,61 @@ void UCAnimNotifyState_Collision::NotifyEnd(USkeletalMeshComponent* MeshComp, UA
 	CheckNull(weapon->GetAttachment());
 
 	weapon->GetAttachment()->OffCollisions();
+}
+
+void UCAnimNotifyState_Collision::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	float FrameDeltaTime)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
+
+	FVector Start = MeshComp->GetSocketLocation(StartSocketName);
+	FVector End = MeshComp->GetSocketLocation(EndSocketName);
+
+	FHitResult hitResult;
+	TArray<TEnumAsByte<EObjectTypeQuery>> objects;
+	objects.Add(EObjectTypeQuery::ObjectTypeQuery3);//ObjectTypeQuery3은 Pawn. Pawn을 추적하기 위해 objects에 추가하였다
+	TArray<AActor*> ignores;//제외시킬 것들을 담을 배열 변수
+
+	bool condition = UKismetSystemLibrary::SphereTraceSingleForObjects(
+		MeshComp,
+		Start,
+		End,
+		140.0f,
+		objects,
+		false,
+		ignores,
+		EDrawDebugTrace::None,
+		hitResult,
+		true
+	);
+
+	if(condition)
+	{		
+		UGameplayStatics::SpawnEmitterAtLocation( // Blood 피 튀기기
+			MeshComp,
+			HitEffectBlood,
+			hitResult.ImpactPoint,
+			UKismetMathLibrary::MakeRotFromXY(hitResult.Normal, hitResult.Normal),
+			FVector(0.4f, 0.4f, 0.4f),
+			true
+		);		
+		UGameplayStatics::SpawnEmitterAtLocation( // 무기 Particle 
+			MeshComp,
+			HitEffectParticleSystem,
+			hitResult.ImpactPoint,
+			UKismetMathLibrary::MakeRotFromX(hitResult.ImpactNormal),
+			ParticleScale,
+			true
+		);		
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation( // 무기 Particle - Niagara
+			MeshComp,
+			HitEffectNiagaraSystem,
+			hitResult.ImpactPoint,
+			UKismetMathLibrary::MakeRotFromX(hitResult.ImpactNormal),
+			NiagaraScale,
+			true
+		);
+	}
+
+
 } 
--- Source/RPG/Notifies/CAnimNotifyState_Collision.h
+++ Source/RPG/Notifies/CAnimNotifyState_Collision.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "NiagaraSystem.h"
 #include "CAnimNotifyState_Collision.generated.h"
 
 UCLASS()
@@ -10,8 +11,30 @@ class RPG_API UCAnimNotifyState_Collision : public UAnimNotifyState
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditAnywhere)
+	FName StartSocketName;
+
+	UPROPERTY(EditAnywhere)
+	FName EndSocketName;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* HitEffectBlood;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* HitEffectParticleSystem;
+
+	UPROPERTY(EditAnywhere)
+	UNiagaraSystem* HitEffectNiagaraSystem;
+
+	UPROPERTY(EditAnywhere)
+	FVector ParticleScale = FVector(1.0f, 1.0f, 1.0f);
+
+	UPROPERTY(EditAnywhere)
+	FVector NiagaraScale = FVector(1.0f, 1.0f, 1.0f);
+
 	FString GetNotifyName_Implementation() const override;
 
 	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
 	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime) override;
 };
